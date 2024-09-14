5bc59dee1b7f945a7648eabd116d636116abf381
gioxavier7 <giovanna.gsx1997@gmail.com>
2024-09-14 10:23:29 -0300

    separando as funções com uma única responsabilidade

--- node/atv-node/exercicio1/modulo/calculoImc.js
+++ node/atv-node/exercicio1/modulo/calculoImc.js
@@ -1,56 +1,66 @@
 /*
 * objetivo: arquivo responsável pela criação de funções e estruturas de repetição 
-* data: 06/09/24
+* data: 13/09/24
 * dev: giovanna
-* versão: 1.0
+* versão: 1.1
 */
 
-// função para validar os dados IMC
-function validarDadosImc(peso, altura){
-    if (peso == '' || altura == ''){
+// Função para validar os dados IMC
+function validarDadosImc(peso, altura) {
+    if (peso === '' || altura === '') {
         console.log("ERRO: é obrigatório a entrada dos dados.")
         return false
-    }
-    else if (isNaN(peso) || isNaN(altura)){
+    } else if (isNaN(peso) || isNaN(altura)) {
         console.log("ERRO: não é permitido a entrada de caracteres.")
         return false
     }
     return true
 }
 
-// função para converter o número para string (toString), substitui a vírgula por ponto, e depois converte de volta para número
-function converterVirgula(valor){
-    return valor.toString().replace(',', '.')
+// Função para converter o número para string, substituir a vírgula por ponto e converter de volta para número
+function converterVirgula(valor) {
+    return parseFloat(valor.toString().replace(',', '.'))
 }
 
-// calculo IMC
-function calcularIMC(peso, altura){
-    let numeroPeso = peso
-    let numeroAltura = altura
-    let imc
+// Função para calcular o IMC
+function calcularIMC(peso, altura) {
+    const numeroPeso = converterVirgula(peso);
+    const numeroAltura = converterVirgula(altura)
+
+    return numeroPeso / (numeroAltura * numeroAltura)
+}
+
+// Função para verificar a categoria do IMC
+function verificarCategoriaImc(imc) {
     let categoriaImc
-    let status = false
 
-    if (validarDadosImc(numeroPeso, numeroAltura)){
-        imc = converterVirgula(numeroPeso) / (converterVirgula(numeroAltura) * converterVirgula(numeroAltura))
-        status = true
+    if (imc < 18.5) {
+        categoriaImc = 'Abaixo do peso'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        categoriaImc = 'Peso normal'
+    } else if (imc >= 25 && imc <= 29.9) {
+        categoriaImc = 'Acima do peso'
+    } else if (imc >= 30 && imc <= 34.9) {
+        categoriaImc = 'Obesidade I'
+    } else if (imc >= 35 && imc <= 39.9) {
+        categoriaImc = 'Obesidade II'
+    } else if (imc >= 40) {
+        categoriaImc = 'Obesidade III'
+    }
 
-        //verificação do IMC
-        if(imc < 18.5){
-            categoriaImc = 'Abaixo do peso'
-        }else if(imc >= 18.5 && imc <= 24.9){
-            categoriaImc = 'Peso normal'
-        }else if(imc >= 25 && imc <= 29.9){
-            categoriaImc = 'Acima do peso'
-        }else if(imc >= 30 && imc <= 34.9){
-            categoriaImc = 'Obesidade I'
-        }else if(imc >= 35 && imc <= 39.9){
-            categoriaImc = 'Obesidade II'
-        }else if(imc >= 40){
-            categoriaImc = 'Obesidade III'
-        }
+    return categoriaImc
+}
+
+// Função principal para processar o cálculo do IMC e exibir o resultado
+function processarImc(peso, altura) {
+    if (validarDadosImc(peso, altura)) {
+        const imc = calcularIMC(peso, altura)
+        const categoriaImc = verificarCategoriaImc(imc)
 
         console.log(`IMC calculado: ${imc.toFixed(2)}\nCategoria: ${categoriaImc}`)
+        return true;
     }
-    return status
+    return false
 }
+
+console.log(processarImc(53, 1.63))
